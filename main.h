61424c6407ffa89cc97f95c100eacbda6629fb8b
Darc99 <lennyyhugoh@gmail.com>
2022-05-20 03:17:47 +0100

    wait adj

--- main.h
+++ main.h
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <sys/wait.h>
+// #include <sys/wait.h>
 #include <sys/stat.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -37,7 +37,7 @@ typedef struct data
 	char **args;
 	int status;
 	int counter;
-	char **_environ;
+	// char **_environ;
 	char *pid;
 } data_shell;
 
